eda59fd2bf635dcbf03e06b146a50ad71ad327b9
Richard Huynh <voxlympha@gmail.com>
2012-11-01 14:30:59 +1100

    More stuff on the user class.

--- SmartHome/Classes/User.cs
+++ SmartHome/Classes/User.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Security.Cryptography;
 
 namespace SmartHome.Classes
 {
     public class User
     {
+
         //Internal username
         public int userID
         {
@@ -33,8 +35,13 @@ namespace SmartHome.Classes
         //Contains the encrypted version of the password created by the one-way hashing
         protected string userPassword //Either make type byte[length of hash] or make sure that the thing spits out a string of set size?
         {
+            
             get { return userPassword; }
-            set { /*Insert one-way hashing here*/ }
+            set
+            {
+                MD5 dodgy = MD5.Create();
+                //dodgy.ComputeHash(value, 0, value.Length);    
+            }
         }
 
         //User contact details ie. Phone number
@@ -42,6 +49,41 @@ namespace SmartHome.Classes
         {
             get { return userContact; }
             set { userContact = value; }
+        }
+
+        public string addUser(int userType, string userName, string userPassword, string userContact)
+        {
+            bool userDetailsFine = true;
+            /*Test for usernames being same as already entered into system
+            foreach(User usersblah of What that userlist is)
+                {
+                    if(usersblah.userName = userName)
+                    {
+                        return false;
+                    }
+                }
+            */
+
+            //Check if user password is correct length
+            if (userPassword.Length < 6)
+            {
+                return "Password length too short (shorter than 6 characters)";
+            }
+
+            //Check if user password contatins at least one letter, number and symbol. (alphanumsymbol)
+
+            //Username 16 max no spaces, password 6 char, at least one letter, number and special, 
+            if (userDetailsFine)
+            {
+                this.userID = 0; //Create new userid here
+                this.userType = userType;
+                this.userName = userName;
+                this.userPassword = userPassword;
+                this.userContact = userContact;
+                return "User account successfully created";
+            }
+
+            return "Unknown error occurred in account creation";
         }
     }
 }
